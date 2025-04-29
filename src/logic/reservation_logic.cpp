c1b42b5eed970f92577d5281acf5488beb29eb9d
Tomderandere <tompbickle@gmail.com>
2025-04-29 11:15:16 +0200

    get next reservation

--- src/logic/reservation_logic.cpp
+++ src/logic/reservation_logic.cpp
@@ -24,14 +24,32 @@ auto insert_pos = std::lower_bound(
     [](const Reservation& a, const Reservation& b) {
         return a.from < b.from; // oder: komplexer, wenn nötig
     });
-    
+
 reservations.insert(insert_pos, reservation);
 }
 
 Reservation get_next_reservation(std::vector<Reservation> const& reservations, std::string const& next_stop, std::vector<std::string> const& stops)
 {
-    // TODO
+if (reservations.empty()|| next_stop.empty()||stops.empty())
+{
+    return Reservation();
+}
+auto next_it = std::find(stops.begin(), stops.end(), next_stop);
+if (next_it==stops.end()){
+    return Reservation();
+}
+int next_index = std::distance(stops.begin(),next_it);
+for( auto const& res : reservations){
+    auto it_to = std::find(stops.begin(), stops.end(), res.to);
+    if (it_to == stops.end()){
+        continue;
+    }
+    int to_index = std::distance(stops.begin(), it_to);
 
+    if (res.to == next_stop || to_index > next_index){
+        return res;
+    }
+}
     return Reservation();
 }
 
